9e1c4e2fd3e7eb2bcdaff70cff1752defd0b3763
fitzgit <fitzcyan@gmail.com>
2014-07-22 18:18:12 -0500

    change 2 added some spo lines in it

--- Calculator/src/SumUp.java
+++ Calculator/src/SumUp.java
@@ -3,6 +3,8 @@ public class SumUp {
 
 	public int add(int a, int b){
 		int sum=a+b;
+		System.out.println("This program sum up two numbers");
+		System.out.println("===============================");
 		System.out.println("the sum of the two numbers is ");
 		return sum;
 	}
